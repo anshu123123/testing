866c7bdd8005c78d01d50d30341e05a18c11c4b0
anshu123123 <77403308+anshu123123@users.noreply.github.com>
2023-10-09 11:36:41 +0000

    9thoct

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -4,3 +4,21 @@ const repocount = 50
 console.log(name+repocount+"value");
 
 console.log("we are so called \" VIKINGS \" of the North ")
+
+let fullname = new String("sudhanshu negi")
+console.log(typeof fullname)
+
+let othername = "pussy,  cat "
+console.log(othername[5])
+
+let fruits = "apple , banana , kiwi"
+let part =  fruits.slice(-12,-10)
+
+console.log(part)
+
+let car = " volvo xc001"
+let car_part = car.substring(3)
+console.log(car_part)
+
+
+
